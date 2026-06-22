37b88032fc953eaa62a120cbc49d03d322ea730c
Golu kumar <dragonarya408@gmail.com>
2026-06-22 20:31:59 +0530

    Add basic calculator functionality in script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+const input = document.getElementsByClassName("value");
+const buttons = document.querySelectorAll("button");
+
+let string = "";
+let arr = Array.from(buttons)
+arr.forEach(button => {
+    button.addEventListener('click',(e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string)
+            document.querySelector('input').value = string
+
+        }
+        else if(e.target.innerHTML == 'AC'){
+            string = ""
+            document.querySelector('input').value = string
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.slice(0,-1)
+            document.querySelector('input').value = string
+        }
+        else{
+            string = string + e.target.innerHTML
+            document.querySelector('input').value = string
+        }
+    })
+})
+
+
+
